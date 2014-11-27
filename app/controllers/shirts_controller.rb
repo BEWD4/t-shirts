4c6bc5acacab3f0fb736a228507a5f04efae85ed
Łukasz Korecki <lukasz@coffeesounds.com>
2014-11-27 20:05:18 +0000

    Handle search + no results

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -5,7 +5,7 @@ class ShirtsController < ApplicationController
 
   def search
     @search = params[:q]
-    # @shirt = Tshirt.all.find { |shirt| shirt.name.downcase.include? @search }
     @shirt = Tshirt.where(  "name LIKE ?", "%#{@search}%" ).first
+    @no_results = !@shirt
   end
 end
